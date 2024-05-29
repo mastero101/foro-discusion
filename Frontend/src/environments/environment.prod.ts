3910baa0d530e28e85b0558b25487becbdd4be98
mastero101 <mastershift_101@hotmail.com>
2024-05-28 20:42:42 -0600

    corregido enviroment incluido Investigando para remover

---
+++ Frontend/src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    production: true,
+    secretKey: 'F_OBd0lnTWxMsy10mvcHQHvUg5yU14hTNyAqylyU2ECwvp2hKpHXREKBKNWKO67u'
+};
---
+++ Frontend/src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: false,
+    secretKey: 'F_OBd0lnTWxMsy10mvcHQHvUg5yU14hTNyAqylyU2ECwvp2hKpHXREKBKNWKO67u'
+};
+  
